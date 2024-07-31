53d9a3bbcb1aff83309f2a868670e8e5df403114
Otaiki1 <otaikisadiq2804@gmail.com>
2024-07-31 01:10:08 +0100

    feat: add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,165 @@
-import Image from "next/image";
+
+import { Button } from "./ui/Button";
+import { Crimson_Text } from "next/font/google";
+import "./page.css";
+import { IBM_Plex_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
+import Image, { StaticImageData } from "next/image";
+import highAngleMan from "./assets/high-angle-man.png"
+import clsx from "clsx";
+import Link from "next/link";
+
+
+const crimsonText = Crimson_Text({ weight: "400" , subsets: ["latin"]});
+const lato = Lato({ weight: "400", subsets: ["latin"] });
+const ibmPlexMono = IBM_Plex_Mono({
+    weight: ["200", "300"],
+    subsets: ["latin"],
+});
 
 export default function Home() {
+    return (
+        <>
+            <main className="relative bg-cover h-screen">
+                <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
+                <div className="relative z-10 h-full w-full py-8 px-16">
+                    <nav className={crimsonText.className}>
+                        <div className="flex justify-between items-center">
+                            <h1 className="tracking-wider text-white z-20">
+                                Privy Proof
+                            </h1>
+                            <Button className="bg-transparent text-white tracking-widest z-20">
+                                Connect Wallet
+                            </Button>
+                        </div>
+                    </nav>
+                    <div className="relative w-100 mt-80 z-20">
+                        <div className={ibmPlexMono.className}>
+                            <h1 className="w-100 text-center text-white text-5xl">
+                                Unlock Privacy Prove Legitimacy
+                            </h1>
+                            <p className="mx-auto w-[52%] text-white text-center text-xl leading-10 tracking-wide mt-5">
+                                Securely sign binding agreements on the
+                                blockchain. Ensure your privacy and authenticate
+                                your commitments with unmatched transparency and
+                                security. Welcome to the future of trust and
+                                accountability.
+                            </p>
+                            <Button className="bg-white w-64 h-16 block mx-auto mt-8">
+                                Get Started
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <div className="bg-black flex flex-col space-y-3 py-3">
+                <div className="p-12 bg-[#1a1919]">
+                    <h1 className="w-full text-center text-white">
+                        {" "}
+                        At Privy Proof
+                    </h1>
+                    <div className={crimsonText.className}>
+                        <p className="text-2xl text-white text-center w-full mt-5">
+                            Privy Proof is a cutting-edge decentralized
+                            application built on the blockchain
+                        </p>
+                    </div>
+                    <Card
+                        image={highAngleMan}
+                        heading="Secure and Binding Agreements:"
+                        body="Privy Proof ensures that all agreements signed on our platform are immutable and legally binding. Once an agreement is signed, it is securely recorded on the blockchain, providing a tamper-proof record that can be verified by all parties involved."
+                    />
+                </div>
+                <Card
+                    image={highAngleMan}
+                    className="flex-row-reverse"
+                    heading="Transparency and Accountability:"
+                    body="Every action taken on Privy Proof is recorded on the blockchain, creating an auditable trail of evidence. This transparency fosters trust between parties and ensures that all agreements are honored and verifiable."
+                />
+                <Card
+                    image={highAngleMan}
+                    heading="AI-Powered Contract Drafting:"
+                    body="Privy Proof incorporates advanced AI technology to assist users in drafting contracts. Our AI system provides suggestions and templates, ensuring that your agreements are comprehensive and legally sound."
+                />
+                <Card
+                    image={highAngleMan}
+                    className="flex-row-reverse"
+                    heading="User-Friendly Interface:"
+                    body="Privy Proof offers an intuitive and user-friendly interface, making it easy for anyone to create, sign, and manage agreements. Whether you're a business professional or an individual, our platform is designed to meet your needs."
+                />
+                <div className="p-12 bg-[#1a1919] flex flex-col space-y-8 justify-center items-center">
+                    <div className="my-24">
+                        <h1
+                            className={`${crimsonText.className} text-4xl text-white`}
+                        >
+                            Why not come onboard?
+                        </h1>
+                        <Button
+                            className={`${crimsonText.className} bg-white w-64 h-16 block tracking-widest text-sm mt-12 mx-auto`}
+                        >
+                            Get Started
+                        </Button>
+                    </div>
+                </div>
+                <div className="py-24">
+                    <div
+                        className={`${ibmPlexMono.className} flex w-[55%] mx-auto text-white justify-between tracking-wider my-12`}
+                    >
+                        <Link href={""}> Contracts</Link>
+                        <Link href={""}>Agreements</Link>
+                        <Link href={""}>Signatures</Link>
+                        <Link href={""}>Closures</Link>
+                    </div>
+                </div>
+                <div
+                    className={`${crimsonText.className} mt-12 text-center text-[#BEB6B6] text-[20px]`}
+                >
+                    2024 @Privy proof
+                </div>
+            </div>
+        </>
+    );
+}
+
+interface CardProps {
+    className?: string;
+    image: StaticImageData;
+    heading: string;
+    body: string;
+}
+
+const Card  = ({className, image, heading, body}: CardProps) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="p-12 bg-[#1a1919]">
+          <div className={clsx("mt-24 flex w-[80%] mx-auto items-center space-x-8", className)}>
+              <div className="w-[30%]">
+                  <Image
+                      src={image}
+                      alt={heading}
+                      className="drop-shadow-[16px_16px_rgba(60,59,59,1)]"
+                      width={300}
+                      height={400}
+                  />
+              </div>
+              <div className="mx-12 px-24 w-[100%]">
+                  <div className="flex flex-col space-y-8">
+                      <h1
+                          className={`${crimsonText.className} text-white text-2xl`}
+                      >
+                          {heading}
+                      </h1>
+                      <p
+                          className={`${lato.className} text-[#BEB6B6] leading-10`}
+                      >
+                          {body}
+                      </p>
+                      <Button className={`${crimsonText.className} bg-white w-64 h-16 block tracking-widest text-sm`}>
+                          Get Started
+                      </Button>
+                  </div>
+              </div>
+          </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
+
